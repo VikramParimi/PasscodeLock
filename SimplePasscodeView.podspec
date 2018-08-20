efa43caaaf9e033c26dda8d68149152f089a8077
Vikram Chowdary <vikramchowdary.102@gmail.com>
2018-08-21 00:01:43 +0530

    Added configurable usage in viewcontroller

--- SimplePasscodeView.podspec
+++ SimplePasscodeView.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name         = "SimplePasscodeView"
-  spec.version      = "0.0.1"
+  spec.version      = "0.0.2"
   spec.license      = { :type => 'MIT' }
   spec.homepage     = "https://github.com/vkrmchowdary/PasscodeView"
   spec.author       = { "Geeko Coco" => "vikramchowdary.102@gmail.com" }
@@ -8,7 +8,11 @@ Pod::Spec.new do |spec|
   spec.description  = <<-DESC
                    SimplePasscodeView mimics iOS Passcode behaviour with more customizable options and can be easily integrated into any project.
                    DESC
-spec.source       = { :git => "https://github.com/vkrmchowdary/PasscodeView.git", :tag => spec.version.to_s}
-  spec.source_files = 'SimplePasscodeView/PasscodeView/*'
+
+  spec.ios.deployment_target  = '11.0'
+  spec.swift_version = '4.1'
+
+  spec.source       = { :git => "https://github.com/vkrmchowdary/PasscodeView.git", :tag => 'v0.0.2'}
+  spec.source_files = 'SimplePasscodeView/PasscodeView/*.swift', 'SimplePasscodeView/PasscodeView/*.xib'
   spec.ios.framework = 'UIKit'
 end
--- SimplePasscodeView/ViewController.swift
+++ SimplePasscodeView/ViewController.swift
@@ -14,16 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         passcodeView.delegate = self
-        passcodeView.pinfillColor = UIColor.green
-        passcodeView.isSecureEntry = false
-        passcodeView.pinfont = UIFont.boldSystemFont(ofSize: 25)
-        passcodeView.pinfontColor = UIColor.blue
+        
+        passcodeView.isSecureEntry = true
+        passcodeView.length = 5
+        passcodeView.keyboardType = .numberPad
+        
+        passcodeView.pinborderColor = UIColor.blue.cgColor
+        passcodeView.pinfillColor = UIColor.lightGray
     }
 }
 
 extension ViewController: SimplePasscodeDelegate {
+    func didDeleteBackward() {
+         //Do whatever you want
+    }
+    
     func didFinishEntering(_ passcode: String) {
+        //Do whatever you want
         print("passcode: \(passcode)")
+        let alert = UIAlertController(title: "Passcode", message: passcode, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
     }
 }
 
