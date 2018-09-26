539cabdc7fb2c39d2533daaf30ffcaed5a81822c
Vikram Chowdary <vikramchowdary.102@gmail.com>
2018-09-26 17:05:24 +0530

    doc

--- SimplePasscodeView/ViewController.swift
+++ SimplePasscodeView/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Extra documentation
+        //More Extra documentation
         passcodeView.delegate = self
         
         passcodeView.isSecureEntry = true
